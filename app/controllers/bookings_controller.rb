7b1c2ab02a878f06612ab6d2da9d3d613983213e
Raymund Werling <raymund.werling@googlemail.com>
2022-11-24 18:06:12 +0100

    test

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -15,16 +15,25 @@ class BookingsController < ApplicationController
   end
 
   def create
+    # @booking = Booking.new(booking_params)
+    # @booking.user = current_user
+    # @booking.lego = Lego.find(params[:lego_id])
+    # @lego = Lego.find(params[:lego_id])
+    # date_dif = Time.diff(Time.parse(@booking.start_date), Time.parse(@booking.end_date))[:day]
+    # calculation = date_dif * (@lego.number_of_parts * 2)
+    # @booking.final_price = calculation
+    # @lego = @booking.lego
+    # @booking.save!
+    # redirect_to @lego
+
     @booking = Booking.new(booking_params)
     @booking.user = current_user
     @booking.lego = Lego.find(params[:lego_id])
-    @lego = Lego.find(params[:lego_id])
     date_dif = Time.diff(Time.parse(@booking.start_date), Time.parse(@booking.end_date))[:day]
-    calculation = date_dif * (@lego.number_of_parts * 2)
+    calculation = date_dif * (@booking.lego.number_of_parts * 2)
     @booking.final_price = calculation
-    @lego = @booking.lego
     @booking.save!
-    redirect_to @lego
+    redirect_to @booking.lego
   end
 
   def show
@@ -35,6 +44,6 @@ class BookingsController < ApplicationController
  private
 
   def booking_params
-    params.require(:booking).permit(:lego, :start_date, :end_date)
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
